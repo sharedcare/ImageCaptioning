d9a2d94f73dc7a5635ce2563e6642526f0627835
sharedcare <sharedcare@hotmail.com>
2018-04-25 21:02:53 -0500

    Update image preprocessing

--- preprocessing/image_processing.py
+++ preprocessing/image_processing.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
-#title           :image_processor.py
-#description     :Preprocessing the images.
-#author          :Yizhen Chen
-#date            :Apr. 24, 2018
-#python_version  :3.6.3
+# title           :image_processor.py
+# description     :Preprocessing the images.
+# author          :Yizhen Chen
+# date            :Apr. 24, 2018
+# python_version  :3.6.3
 #==============================================================================
 import numpy as np
 from keras.preprocessing.image import load_img, img_to_array
 from keras.applications.inception_v3 import preprocess_input
 from tqdm import tqdm
 from keras.preprocessing.image import list_pictures
+
 
 class ImagePreprocessor(object):
 
@@ -18,20 +19,19 @@ class ImagePreprocessor(object):
         self._is_training = is_training
 
     def _process_image(self, img_dir):
-        '''Load and resize the image.
+        # Load and resize the image.
+        #
+        # This returns the image as a numpy-array.
+        #
+        # Args:
+        #     img_dir: the directory of the image file.
+        #
+        # Returns:
+        #     A preprocessed numpy array.
 
-        This returns the image as a numpy-array.
-
-        Args:
-            img_dir: the directory of the image file.
-            _image_size: the target size of image
-
-        Returns:
-            A preprocessed numpy array.
-        '''
         img = load_img(img_dir, target_size=self._img_size)
         img_array = img_to_array(img)
-        #if self._is_training:
+        # if self._is_training:
         #    img_array = distort_img(img_array)  # Add distort_img method
         return preprocess_input(img_array)
 
@@ -52,17 +52,16 @@ class ImagePreprocessor(object):
             end_index = i + batch_size
             image_batch = images[i:end_index]
             processed_batch = self.process_batch(image_batch)
-            if end_index > num_images:
-                batch_size = end_index - i
             pbar.update(batch_size)
-            processed_img.append(processed_batch)
-
+            processed_img.extend(processed_batch)
+        pbar.update(5)
         return np.array(processed_img)
+
 
 image_processor = ImagePreprocessor(is_training=True, img_size=(299, 299))
 data_dir = "../flickr8k/Flicker8k_Dataset/"
 image_files = list_pictures(data_dir)
-print("Preprocessing images...")
+print("Preprocessing images...\n")
 array = image_processor.process_images(image_files)
-print("Images preprocessed.")
+print("\nImages preprocessed.")
 print(array.shape)
