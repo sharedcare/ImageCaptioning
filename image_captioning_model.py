76397f6543904085e2d494f89eeb6205c27cd9fe
sharedcare <sharedcare@hotmail.com>
2018-04-25 22:19:52 -0500

    Add losses and callback

--- image_captioning_model.py
+++ image_captioning_model.py
@@ -4,13 +4,17 @@
 # author          :Yizhen Chen
 # date            :Apr. 24, 2018
 # python_version  :3.6.3
-#==============================================================================
+# ==============================================================================
 import numpy as np
 from keras.applications.inception_v3 import InceptionV3
 from keras.layers import Input, BatchNormalization, Embedding, Dense, GRU, LSTM
 from keras.models import Model
 from keras import backend as K
 from keras.regularizers import l1, l2, l1_l2
+from keras.optimizers import Adam, RMSprop
+import tensorflow as tf
+
+from ..losses import sparse_cross_entropy as loss
 
 class ImageCaptioningModel(object):
 
@@ -24,6 +28,7 @@ class ImageCaptioningModel(object):
                  embedding_size=128,
                  rnn_activation='tanh',
                  cnn_model=InceptionV3,
+                 optimizer=RMSprop,
                  reg_l1=None,
                  reg_l2=None,
                  num_word=None):
@@ -35,6 +40,8 @@ class ImageCaptioningModel(object):
         self._word_size = num_word
         self._hidden_dim = hidden_dim
         self._cnn_model = cnn_model
+        self._eta = learning_rate
+        self._optimizer = optimizer
         if reg_l1 and reg_l2:
             self._regularizer = l1_l2(reg_l1, reg_l2)
         elif reg_l1:
@@ -43,7 +50,6 @@ class ImageCaptioningModel(object):
             self._regularizer = l2(reg_l2)
         else:
             self._regularizer = None
-
 
     def _cell(self):
         if self._rnn_mode == 'gru' or 'GRU':
@@ -55,8 +61,30 @@ class ImageCaptioningModel(object):
 
         return cell
 
+    def build_model(self):
+        image_input, image_embedding = self._image_embedding()
+        decoder_input, decoder_embedding = self._word_embedding()
+        Model(inputs=image_input,
+              outputs=image_embedding)
+        decoder_output = self._decoder_model(decoder_input)
+        decoder_target = tf.placeholder(dtype='int32', shape=(None, None))
+        decoder_model = Model(inputs=[image_input, decoder_input],
+                              outputs=decoder_output)
+        decoder_model.compile(optimizer=self._optimizer(lr=self._eta),
+                              loss=loss,
+                              target_tensors=[decoder_target])
+
     def _activation_fn(self):
-        activation_fns = ['softmax', 'elu', 'selu', 'softplus', 'softsign', 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear']
+        activation_fns = ['softmax',
+                          'elu',
+                          'selu',
+                          'softplus',
+                          'softsign',
+                          'relu',
+                          'tanh',
+                          'sigmoid',
+                          'hard_sigmoid',
+                          'linear']
         if self._rnn_activation not in activation_fns:
             raise ValueError('rnn_activation must be a valid activation function')
         else:
@@ -86,11 +114,11 @@ class ImageCaptioningModel(object):
 
     def _decoder_model(self, decoder_input):
         hidden_layer = self._cell()(units=self._rnn_state_size,
-                            dropout=self._drop_rate,
-                            recurrent_dropout=self._drop_rate,
-                            return_sequences=True,
-                            recurrent_regularizer=self._regularizer,
-                            implementation=2)
+                                    dropout=self._drop_rate,
+                                    recurrent_dropout=self._drop_rate,
+                                    return_sequences=True,
+                                    recurrent_regularizer=self._regularizer,
+                                    implementation=2)
 
         decoder_dense = Dense(self._word_size,
                               activation=self._rnn_activation,
---
+++ losses.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import tensorflow as tf
+
+
+def sparse_cross_entropy(y_true, y_pred):
+    """
+    Calculate the cross-entropy loss between y_true and y_pred.
+
+    y_true is a 2-rank tensor with the desired output.
+    The shape is [batch_size, sequence_length] and it
+    contains sequences of integer-tokens.
+
+    y_pred is the decoder's output which is a 3-rank tensor
+    with shape [batch_size, sequence_length, num_words]
+    so that for each sequence in the batch there is a one-hot
+    encoded array of length num_words.
+    """
+
+    # Calculate the loss. This outputs a
+    # 2-rank tensor of shape [batch_size, sequence_length]
+    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_true,
+                                                          logits=y_pred)
+
+    # Keras may reduce this across the first axis (the batch)
+    # but the semantics are unclear, so to be sure we use
+    # the loss across the entire 2-rank tensor, we reduce it
+    # to a single scalar with the mean function.
+    loss_mean = tf.reduce_mean(loss)
+
+    return loss_mean
